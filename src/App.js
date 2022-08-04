41c1677e6a4092652ac88d95d8e97154863185c0
Carson-Zuniga <carsonzuniga@gmail.com>
2022-08-04 10:39:16 -0500

    implemented all features

---
+++ src/App.js
@@ -0,0 +1,210 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import {Container, Row, Col, Button, Form} from "react-bootstrap/";
+import GoogleAuth from './GoogleAuth';
+  
+const credentials = require('./credentials.json');
+
+const SPOTIFY_CLIENT_ID = credentials.SPOTIFY_CLIENT_ID
+const SPOTIFY_REDIRECT_URI = credentials.SPOTIFY_REDIRECT_URI
+const SPOTIFY_AUTH_ENDPOINT = credentials.SPOTIFY_AUTH_ENDPOINT
+const SPOTIFY_RESPONSE_TYPE = credentials.SPOTIFY_RESPONSE_TYPE
+const SPOTIFY_API_ENDPOINT = credentials.SPOTIFY_API_ENDPOINT
+const SPOTIFY_PLAYLIST_SCOPE = credentials.SPOTIFY_PLAYLIST_SCOPE
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            spotifytoken: "",
+            searchKey: "",
+            playlistSongs: [],
+            playlists: [],
+            chosenPlaylistID: "",
+            chosenPlaylistName: ""
+        }
+        this.spotifyLogout = this.spotifyLogout.bind(this);
+        this.getPlaylists = this.getPlaylists.bind(this);
+        this.getSongsFromPlaylist = this.getSongsFromPlaylist.bind(this);
+        
+    }
+
+    componentDidMount() {
+        const hash = window.location.hash
+        let spotifytoken = window.localStorage.getItem("spotifytoken")
+
+        if (!spotifytoken && hash) {
+            spotifytoken = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+            window.location.hash = ""
+            window.localStorage.setItem("spotifytoken", spotifytoken)
+        }
+
+        this.setState({spotifytoken: spotifytoken});
+    }
+
+    spotifyLogout() {
+        this.setState({spotifytoken: ""})
+        window.localStorage.removeItem("spotifytoken")
+        window.location.href= SPOTIFY_REDIRECT_URI.replace('/spotify-callback', '');
+    }
+
+    getSongsFromPlaylist = async(event) => {
+        event.preventDefault()
+        try {
+            var new_playlist_songs = [];
+            let i = 0;
+            this.setState({playlistSongs:[]})
+            while(true) {
+                let {data} = await axios.get(`${SPOTIFY_API_ENDPOINT}/playlists/${this.state.searchKey}/tracks`, {
+                    headers: {
+                        Authorization: `Bearer ${this.state.spotifytoken}`
+                    },
+                    params: {
+                        'limit': 100,
+                        'offset': 100 * i
+                    }
+                })
+                if(data.items.length === 0)
+                    break;
+
+                for(var j = 0; j < data.items.length; j++) {
+                    new_playlist_songs.push(data.items[j].track);
+                }
+                i += 1;
+            }
+            this.setState({playlistSongs: new_playlist_songs})
+        } catch(error) {
+            alert(`Could not get playlist with ID ${this.state.searchKey}`)
+        }
+    }
+
+    getPlaylists = async(event) => {
+        event.preventDefault()
+        try {
+            const {data} = await axios.get(`${SPOTIFY_API_ENDPOINT}/me/playlists`, {
+                headers: {
+                    Authorization: `Bearer ${this.state.spotifytoken}`
+                },
+                params: {
+                    'limit': 50
+                }
+            })    
+            this.setState({playlists: data.items});
+        } catch(error) {
+            alert('Could not get your playlists');
+        }
+    }
+
+
+    render() {
+        const renderSpotifyLoginButton = () => {
+            if(this.state.spotifytoken) {
+                return (<Button onClick={this.spotifyLogout} className="spotify-button">Logout of Spotify</Button>);
+            }
+            else {
+                return (<a href={`${SPOTIFY_AUTH_ENDPOINT}?client_id=${SPOTIFY_CLIENT_ID}&redirect_uri=${SPOTIFY_REDIRECT_URI}&response_type=${SPOTIFY_RESPONSE_TYPE}&scope=${SPOTIFY_PLAYLIST_SCOPE}`}>
+                            <Button className="spotify-button">Login to Spotify</Button>
+                        </a>
+                        );
+            }
+        }
+        const renderGetPlaylistsButton = () => {
+            return (<Button onClick={this.getPlaylists} className="spotify-button">Get All Playlists</Button>);
+        }
+        const renderPlaylists = () => {
+            if(this.state.playlists.length && this.state.spotifytoken) {
+                return (
+                    <Container>
+                        <Form onSubmit={(e) => {e.preventDefault();}}>
+                            Please Select Your Playlist
+                            {this.state.playlists.map(playlist => (
+                                <div key={playlist.id} className="playlist">
+                                    <Form.Check type="radio" id={playlist.id} label={playlist.name} value={playlist.name} name="chosen_playlist" onChange={(e) => {this.setState({chosenPlaylistID: e.target.id}); this.setState({chosenPlaylistName: e.target.value})}}/>
+                                </div>
+                            ))}
+                        </Form>
+                    </Container>
+                );
+            }
+            else {
+                return (<Container></Container>);
+            }
+        }
+        const renderSearchBar = () => {
+            return (
+                <Container>
+                    {this.state.chosenPlaylistName ? <Row>Chosen Playlist Name: {this.state.chosenPlaylistName}</Row> : <></>}
+                    {this.state.chosenPlaylistID ? <Row>Chosen Playlist ID: {this.state.chosenPlaylistID}</Row> : <></>}
+                    <Row>
+                        <Form onSubmit={this.getSongsFromPlaylist} id="search-bar">
+                            <p>Enter Playlist ID:</p>
+                            <Form.Control type="sm" onChange={e => this.setState({searchKey: e.target.value})} placeholder={this.state.chosenPlaylistID ? "Try copying the above ID. ^" : "Copy your playlist ID here."}/>
+                            <Button type="submit">Search</Button>
+                        </Form>
+                    </Row>
+                </Container>
+            );
+        }
+        const renderPlaylistSongs = () => {
+            if(this.state.playlistSongs.length > 0) {
+                return (
+                    <Container id="song-list">
+                        <p>Your songs:</p>
+                        {this.state.playlistSongs.map(song => (
+                        <Container className="songContainer" key={song.id}>
+                            <Row>
+                                <Col md={4}>
+                                    {song.album.images[0].url ? <img width={"50%"} src={song.album.images[0].url } alt={song.album.name}/> : <>No Image</>}
+                                </Col>
+                                <Col md={8}>
+                                    <Row><p className="song-title">"{song.name}" by {song.artists[0].name}</p></Row>
+                                    <Row><a href={`https://www.youtube.com/results?search_query=${song.name}+${song.artists[0].name}`} target="_blank">Search on YouTube</a></Row>
+                                </Col>
+                            </Row>
+                        </Container>
+                        ))}
+                    </Container>
+                );
+            }
+            else {
+                return (<Container></Container>);
+            }
+        }
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src="images/logo.png" width="10%"/>
+                    <h1>SpotifyToYouTube</h1>
+                    <Container>
+                        <Row>
+                            <Col md={6}>
+                                <Row>
+                                    {renderSpotifyLoginButton()}
+                                    {renderGetPlaylistsButton()}
+                                </Row>
+                            </Col>
+                            <Col md={6}>
+                                <GoogleAuth songs={this.state.playlistSongs} playlistName={this.state.chosenPlaylistName}/>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                
+                                {renderPlaylists()}
+                            </Col>
+                            <Col>
+                                {renderSearchBar()}
+                                {renderPlaylistSongs()}
+                            </Col>
+                        </Row>
+                    </Container>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/GoogleAuth.js
@@ -0,0 +1,209 @@
+import React from 'react'
+import { Button, Container, Row, Col } from 'react-bootstrap';
+
+const credentials = require('./credentials.json');
+
+const YOUTUBE_CLIENT_ID = credentials.YOUTUBE_CLIENT_ID;
+const YOUTUBE_API_KEY = credentials.YOUTUBE_API_KEY;
+
+class GoogleAuth extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isSignedIn: null,
+            playlistID: "",
+            accessToken: ""
+        }
+
+        this.onAuthChange = this.onAuthChange.bind(this);
+        this.onSignInClick = this.onSignInClick.bind(this);
+        this.onSignOutClick = this.onSignOutClick.bind(this);
+        this.createPlaylist = this.createPlaylist.bind(this);
+        this.deletePlaylist = this.deletePlaylist.bind(this);
+        this.addToPlaylist = this.addToPlaylist.bind(this);
+        this.getVideoID = this.getVideoID.bind(this);
+        this.createPlaylistAndPopulate = this.createPlaylistAndPopulate.bind(this);
+    }
+
+    componentDidMount() {
+        window.gapi.load('client:auth2',() => {
+            window.gapi.client.init({
+                clientId: YOUTUBE_CLIENT_ID,
+                scope: "https://www.googleapis.com/auth/youtube.force-ssl",
+                plugin_name: "SpotifyToYouTube"
+            }).then(()=>{
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({isSignedIn:this.auth.isSignedIn.get()});
+                this.auth.isSignedIn.listen(this.onAuthChange);
+            })
+        })
+    }
+
+    onAuthChange() {
+        this.setState({isSignedIn: this.auth.isSignedIn.get()});
+        this.setState({accessToken: this.auth.currentUser.get().getAuthResponse().access_token});
+        console.log(this.state.accessToken)
+    }
+
+    onSignInClick() {
+        this.auth.signIn();
+    }
+    
+    onSignOutClick() {
+        this.auth.signOut();
+    }
+
+    createPlaylist = async () => {
+        try {
+            var response = await window.gapi.client.request({
+                'path': `youtube/v3/playlists?part=snippet&key=${YOUTUBE_API_KEY}`,
+                'method': 'POST',
+                'headers': {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + this.state.accessToken
+                },
+                'body': {
+                    "snippet": {
+                        "title": this.props.playlistName
+                    }
+                }
+            }).then(response => {
+                return response;
+            });
+            this.setState({playlistID: response.result.id})
+        } catch(error) {
+            console.log('Error creating playlist');
+        }
+    }
+
+    deletePlaylist = async () => {
+        try {
+            var response = await window.gapi.client.request({
+                'path': `youtube/v3/playlists?id=${this.state.playlistID}&key=${YOUTUBE_API_KEY}`,
+                'method': 'DELETE',
+                'headers': {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + this.state.accessToken
+                }
+            }).then(response => {
+                return response;
+            });
+            console.log('deleted playlist ', this.state.playlistID);
+        } catch(error) {
+            console.log('Error deleting playlist ', this.state.playlistID);
+        }
+        this.setState({playlistID: ""});
+    }
+
+    addToPlaylist = async (videoID) => {
+        if(this.state.playlistID !== "") {
+            try {
+                var response = await window.gapi.client.request({
+                    'path': `youtube/v3/playlistItems?part=snippet&key=${YOUTUBE_API_KEY}`,
+                    'method': 'POST',
+                    'headers': {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer ' + this.state.accessToken
+                    },
+                    'body': {
+                        "snippet": {
+                            "playlistId": this.state.playlistID,
+                            "resourceId": {
+                                "kind": "youtube#video",
+                                "videoId": videoID
+                            }
+                        }
+                    }
+                });
+                return true;
+            } catch(error) {
+                console.log(`error adding ${videoID} to playlist`);
+                return false;
+            }
+        }
+    }
+
+    getVideoID = async (query) => {
+        try {
+            let maxResults = "1"
+            var response = await window.gapi.client.request({
+                'path': `youtube/v3/search?part=snippet&maxResults=${maxResults}&q=${query.trim().replaceAll(" ", "%20")}&type=video&key=${YOUTUBE_API_KEY}`,
+                'method': 'GET',
+                'headers': {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + this.state.accessToken
+                }
+            }).then(response => {
+                return response;
+            })
+            return response.result.items[0].id.videoId;
+        } catch(error) {
+            console.log(`error getting video id for ${query}`)
+            return null;
+        }
+    }
+
+    createPlaylistAndPopulate = async (playlistSongs) => {
+        if(playlistSongs.length > 0) {
+            await this.createPlaylist();
+            for(var i = 0; i < playlistSongs.length; i++) {
+                let song = playlistSongs[i];
+                let video_id = await this.getVideoID(`${song.name} ${song.artists[0].name}`);
+                if(video_id) {
+                    let added = await this.addToPlaylist(video_id);
+                    if(!added) {
+                        this.deletePlaylist();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    render() {
+        const renderAuthButton = () => {
+            if(this.state.isSignedIn === null) {
+                return null
+            }
+            else if (this.state.isSignedIn) {
+                return (
+                    <Button onClick={this.onSignOutClick} className="youtube-button">
+                        Sign Out of Google
+                    </Button>
+                );
+            }
+            else {
+                return (
+                    <Button onClick={this.onSignInClick} className="youtube-button">
+                        Sign In with Google
+                    </Button>
+                );
+            }
+        }
+
+        const renderCreatePlaylistButton = () => {
+            if(this.state.isSignedIn === null) {
+                return null
+            }
+            else if (this.state.isSignedIn) {
+                return (
+                    <Button onClick={(e) => {e.preventDefault(); this.createPlaylistAndPopulate(this.props.songs);}} className="youtube-button">
+                        Create YouTube Playlist
+                    </Button>
+                );
+            }
+        }
+
+        return (
+            <Container>
+                <Row>
+                    {renderAuthButton()}
+                    {renderCreatePlaylistButton()}
+                </Row>
+            </Container>
+        );
+    }
+}
+
+export default GoogleAuth;
